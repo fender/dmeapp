e43802aa6017840c336da3340a8c81f400941389
Joe Fender <drupaljoe@gmail.com>
2014-07-09 15:40:13 +0100

    switches phantomjs to use lowercase string to solve travis issues

--- Gruntfile.js
+++ Gruntfile.js
@@ -200,7 +200,7 @@ module.exports = function(grunt) {
           'config.js',
           'src/app/**/*.js',
         ],
-        browsers: ['PhantomJS'],
+        browsers: ['phantomjs'],
         frameworks: ['jasmine'],
       },
       dev: {
